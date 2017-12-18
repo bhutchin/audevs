873efc7995e8201cd3ff58437a2ab04893872eb1
brent.hutchin <brent.hutchin@gmail.com>
2017-12-19 08:55:11 +1100

    cleaned up to run on reduced chunk size

--- wav_matrix.py
+++ wav_matrix.py
@@ -1,62 +1,66 @@
-import wave
 from struct import unpack
 import pyaudio
-import sys
-import time
-import alsaaudio as aa
 import numpy as np
 
-
-print "Top goes here"
-print ""
-print ""
-print ""
-print ""
-print ""
-print ""
-print "Bottom goes here"
-#time.sleep(7)
-
-WAV_FILE = wave.open('/home/perses/dev/audevs/audio.wav', 'r')
-SAMPLE_RATE = WAV_FILE.getframerate()
-NO_CHANNELS = WAV_FILE.getnchannels()
-CHUNK = 1024
-
+CHUNK = 256
 FORMAT = pyaudio.paInt32
-CHANNELS = 1
+CHANNELS = 2
 RATE = 44100
-p = pyaudio.PyAudio()
+P = pyaudio.PyAudio()
 
-output = aa.PCM(aa.PCM_PLAYBACK, aa.PCM_NORMAL)
-output.setchannels(NO_CHANNELS)
-output.setrate(SAMPLE_RATE)
-output.setformat(aa.PCM_FORMAT_S16_LE)
-output.setperiodsize(CHUNK)
 
-stream = p.open(
-    format=FORMAT,
-    channels=CHANNELS,
-    rate=RATE,
-    input=True,
-    output=True,
-    frames_per_buffer=CHUNK,
-    input_device_index=6
-    )
+def gen_device():
+    """
+    Returns:
+         input_index(int):
+         output_index(int):
+    """
+    import re
+    dev_count = P.get_device_count()
+    i = 0
+    input_index = ''
+    output_index = ''
+    while i < dev_count:
+        dev_info = P.get_device_info_by_index(i)
+        if re.search('Loopback', dev_info['name']):
+            input_index = dev_info['index']
+        if re.search("\Adefault", dev_info['name']):
+            output_index = dev_info['index']
+        i = i + 1
 
-out_stream = p.open(
-    format=FORMAT,
-    channels=2,
-    rate=RATE,
-    input=True,
-    output=True,
-    frames_per_buffer=CHUNK,
-    input_device_index=6
-    )
+    if input_index == '':
+        print "No loopback exists exiting"
+        exit(1)
+    return input_index, output_index
 
-if len(sys.argv) < 2:
-    pass
-else:
-    WAV_FILE = wave.open(sys.argv[1], 'r')
+
+def gen_stream(input_device, output_device):
+    """
+
+    :param input_device:
+    :param output_device:
+    :return:
+    """
+    in_stream = P.open(
+        format=FORMAT,
+        channels=CHANNELS,
+        rate=RATE,
+        input=True,
+        output=True,
+        frames_per_buffer=CHUNK,
+        input_device_index=input_device
+        )
+
+    out_stream = P.open(
+        format=FORMAT,
+        channels=2,
+        rate=RATE,
+        input=True,
+        output=True,
+        frames_per_buffer=CHUNK,
+        input_device_index=output_device
+        )
+    return in_stream, out_stream
 
 
 def piff(val):
@@ -66,10 +70,11 @@ def piff(val):
     Returns:
 
     """
-    return int(2 * CHUNK * val / SAMPLE_RATE)
+    return int(2 * CHUNK * val / (RATE / (1024 / CHUNK)))
+    #return int(2 * CHUNK * val / (RATE / 4))
 
 
-def calculate_levels(data, chunk, sample_rate):
+def calculate_levels(data):
     """
     Var:
         data:
@@ -85,7 +90,6 @@ def calculate_levels(data, chunk, sample_rate):
     fourier = np.fft.rfft(data)
     fourier = np.delete(fourier, len(fourier)-1)
     power = np.abs(fourier)
-
     matrix[0] = int(np.mean(power[piff(0):piff(30):1]))
     matrix[1] = int(np.mean(power[piff(30):piff(80):1]))
     matrix[2] = int(np.mean(power[piff(80):piff(190):1]))
@@ -94,31 +98,42 @@ def calculate_levels(data, chunk, sample_rate):
     matrix[5] = int(np.mean(power[piff(800):piff(5000):1]))
     matrix[6] = int(np.mean(power[piff(5000):piff(10000):1]))
     matrix[7] = int(np.mean(power[piff(10000):piff(20000):1]))
-    matrix = np.divide(np.multiply(matrix, 1), 1000000)
+    #print matrix
+    matrix = [x / 16000 for x in matrix]
+    #matrix = np.divide(np.multiply(matrix, 1), 1000000)
+    matrix = np.array(matrix)
+    #print matrix
     matrix = matrix.clip(0, 8)
+    #print matrix
     return matrix
 
 
-#RAW_DATA = WAV_FILE.readframes(CHUNK)
-RAW_DATA = stream.read(CHUNK)
-while RAW_DATA != '':
+def draw_matrix():
     """
-    """
-    out_stream.write(RAW_DATA)
-    display = calculate_levels(RAW_DATA, CHUNK, SAMPLE_RATE)
-    rows = [[], [], [], [], [], [], [], []]
-    j = 0
-    for i in display:
-        k = 0
-        while k < int(i):
-            rows[j].append("X")
-            k = k + 1
-        while len(rows[j]) < 8:
-            rows[j].append(" ")
 
-        j = j + 1
-    rotate = reversed(zip(*rows[::1]))
-    for y in rotate:
-        print y
-    #RAW_DATA = WAV_FILE.readframes(CHUNK)
-    stream.read(CHUNK)
+    :return:
+    """
+    data = in_stream.read(CHUNK)
+    while data != '':
+        data = in_stream.read(CHUNK)
+        out_stream.write(data)
+        display = calculate_levels(data)
+        rows = [[], [], [], [], [], [], [], []]
+        j = 0
+        for i in display:
+            k = 0
+            while k < int(i):
+                rows[j].append("X")
+                k = k + 1
+            while len(rows[j]) < 8:
+                rows[j].append(" ")
+            j = j + 1
+            rotate = reversed(zip(*rows[::1]))
+            for l in rotate:
+                print l
+
+
+if __name__ == '__main__':
+    input_device, output_device = gen_device()
+    in_stream, out_stream = gen_stream(input_device, output_device)
+    draw_matrix()
