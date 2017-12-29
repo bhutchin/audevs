71d0f706df5ae930214a5ed72b04e3b9116fda1f
brent.hutchin <brent.hutchin@gmail.com>
2017-12-30 09:55:09 +1100

    updated to include luma

--- gpio.py
+++ gpio.py
@@ -1,1 +1,18 @@
-import RPi.GPIO
+from luma.core.interface.serial import spi, noop
+from luma.core.render import canvas
+from luma.led_matrix.device import max7219
+import time
+
+
+def create_device():
+    serial = spi(port=0, device=0, gpio=noop())
+    device = max7219(serial)
+    return device
+
+
+for y in range(8):
+     for x in range(8):
+         with canvas(device) as draw:
+             draw.point((x,y), fill="green")
+         time.sleep(0.5)
+
